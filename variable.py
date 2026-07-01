f203476a21ffa99f00ca53220f4eaf099ad9a342
onkar-code-tech <onkarjanrao726@gmail.com>
2026-07-01 09:18:43 +0530

    Initial commit

---
+++ variable.py
@@ -0,0 +1,21 @@
+# What is variable:
+'''variable is like a container that holds data. '''
+'''Creating a variable is like creating a placeholder in memory and assigning it some value.'''
+'''You create a variable by assigning a value using the '=' operator'''
+# For example:
+a = "onkar"
+b = 20
+c = True
+d = None
+
+# Variable Types
+'''Python automatically determines the type of data:'''
+# Example:
+
+x = 10          # Integer (int)
+y = 3.14        # Float (float)
+name = "John"   # String (str)
+is_student = True  # Boolean (bool)
+
+print(a)
+print(b)
---
+++ what-is.py
@@ -0,0 +1,27 @@
+# What is python:
+
+'''Python is a programming language used to give instructions to a computer.
+Just like English is used to communicate with people, Python is used to communicate with computers.
+'''
+# With Python, you can build:
+
+'''
+* Websites
+* Games
+* AI applications
+* Automation scripts
+* Data analysis tools
+* Mobile and desktop applications
+
+'''
+# First code in python (Example1)
+
+print("Hello World")
+# Output: Hello World
+
+'''
+* print() is a function.
+* It tells Python to display something on the screen.
+* "Hello World" is a string (text).
+* String writen in "..."
+'''
